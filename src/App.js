ad4043f2850a60eb7f36e86b6f00a2929b975fa2
alihusnain4190 <alihusnain4190@gmail.com>
2021-02-05 13:17:22 +0000

    complete random person

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.scss";
+import Person from "./Component/Person";
+function App() {
+  return (
+    <div className="App">
+      <Person />
+    </div>
+  );
+}
+
+export default App;
